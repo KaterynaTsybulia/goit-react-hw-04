1a4f056221bd404f8bc8304454cacaa17c643068
Katerina <ekaterina041117@gmail.com>
2024-10-12 19:21:32 +0300

    1/a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-// import { getPhotos } from '../src/image-api';
+import { useEffect, useState } from 'react';
+import { getPhotos } from '../src/image-api';
 
 import SearchBar from "./components/SearchBar/SearchBar";
 
@@ -11,9 +11,35 @@ import SearchBar from "./components/SearchBar/SearchBar";
 
 export default function App() {
   const [query, setQuery] = useState ("");
+  const [page, setPage] = useState (1);
+  const [images, setImages] = useState ([]);
+  const [isloading, setIsloading] = useState(false);
+  const [error, setEroor] = useState(null);
+
+
+  useEffect (() => {
+    if(!query) {
+      return;
+    }
+    const fetchData = async () => {
+    setIsloading(true);
+    try {
+      const data = await getPhotos(query, page);
+      console.log(data);
+    } catch (error) {
+      setEroor(error);
+    } finally {
+      setIsloading(false);
+    }
+}
+fetchData();
+  },[page, query])
+
+  
   const onHandleSubmit = (value) =>{
     setQuery(value);
   }
+  
 
 return (
   <SearchBar onSubmit={onHandleSubmit}/>
